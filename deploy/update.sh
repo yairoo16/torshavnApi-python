7896851edbe1470ec31afb3fcce0be3d290db4e4
Benyair Orellana <ben.orellana@gmail.com>
2020-03-31 21:07:36 -0400

    removed requirements from update sh

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,7 +7,6 @@ PROJECT_BASE_PATH='/usr/local/apps/torshavn_api'
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-$PROJECT_BASE_PATH/requirements.txt
 supervisorctl restart torshavn_api
 
 echo "DONE! :)"
