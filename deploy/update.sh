2742ba6b88e6eb90759939e0817b68bc611a98fe
Benyair Orellana <ben.orellana@gmail.com>
2020-03-31 21:06:40 -0400

    added requirements to update sh

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,6 +7,7 @@ PROJECT_BASE_PATH='/usr/local/apps/torshavn_api'
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+$PROJECT_BASE_PATH/requirements.txt
 supervisorctl restart torshavn_api
 
 echo "DONE! :)"
